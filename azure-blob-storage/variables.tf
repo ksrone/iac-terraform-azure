c846ae57612b8bce151c592991c6caba54bd1626
Marcin <104536446+ksrone@users.noreply.github.com>
2023-05-23 16:08:16 +0200

    Create variables.tf

---
+++ azure-blob-storage/variables.tf
@@ -0,0 +1,9 @@
+variable "resource_group_name" {
+  description = "Resource Group Name"
+  type        = string
+}
+
+variable "resource_group_location" {
+  description = "Resource Group Location"
+  type        = string
+}
